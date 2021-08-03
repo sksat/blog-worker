68d5fedc1b4e5fc5288901f5f9c943128d6f45bd
sksat <sksat@sksat.net>
2021-08-03 15:09:37 +0900

    CI/CDして優勝　セカンドシーズン (#13)
    * add: actionlint workflow
    
    * fix: deploy trigger
    
    * add: renovate.json validate workflow
    
    * add: renovate automerge config
    
    * add: prettier format on GitHub Actions when PR
    
    * fix: prettier format workflow trigger
    
    * format: prettier
    
    * fix: CI
    
    * add: test to CI
    
    * fix: typo
    
    * fix: prettier format workflow trigger
    
    * fix: CI trigger
    
    Co-authored-by: sksat <sksat@users.noreply.github.com>

--- worker/worker.js
+++ worker/worker.js
@@ -1,7 +1,8 @@
-const FONT_URL = "https://cdn.plusminus.io/font/webkoruri/20140628/WebKoruri.ttf";
+const FONT_URL =
+  "https://cdn.plusminus.io/font/webkoruri/20140628/WebKoruri.ttf";
 
-addEventListener('fetch', event => {
-	event.respondWith(handleRequest(event.request))
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
 });
 
 /**
@@ -9,28 +10,27 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-	const url = new URL(request.url);
-	//console.log(url);
+  const url = new URL(request.url);
+  //console.log(url);
 
-    const { ogp_image } = wasm_bindgen;
-    await wasm_bindgen(wasm);
+  const { ogp_image } = wasm_bindgen;
+  await wasm_bindgen(wasm);
 
-	const fr = new Request(FONT_URL, {headers: request.headers});
+  const fr = new Request(FONT_URL, { headers: request.headers });
 
-	const f = fetch(fr, {cf: {}})
-		.then(async res => {
-			//console.log(res);
-			const buff = await res.arrayBuffer();
-			const b = new Uint8Array(buff);
+  const f = fetch(fr, { cf: {} }).then(async (res) => {
+    //console.log(res);
+    const buff = await res.arrayBuffer();
+    const b = new Uint8Array(buff);
 
-			//console.log(buff);
-			//console.log(b);
+    //console.log(buff);
+    //console.log(b);
 
-			const ogp = ogp_image(b);
-			//console.log(ogp);
-			return new Response(ogp, {status: 200, "content-type": "image/png"});
-		});
-	return await f;
+    const ogp = ogp_image(b);
+    //console.log(ogp);
+    return new Response(ogp, { status: 200, "content-type": "image/png" });
+  });
+  return await f;
 
-	//return new Response(ogp_image(f.body), {status: 200, "content-type": "image/png"});
+  //return new Response(ogp_image(f.body), {status: 200, "content-type": "image/png"});
 }
